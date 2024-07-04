6398324205ae96413c91ff8ec5e9fafdda1b9972
brillyK <rkdud9798@naver.com>
2024-07-04 12:08:14 +0900

    section2

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const header = document.getElementById('header')
     const section2 = document.getElementById('section2')
     const horizontalScroll = document.getElementById('horizontalScroll')
+    const imgRotate = document.querySelector('.img-rotate img')
 
     horizontalScroll.addEventListener('wheel', function (event) {
         event.preventDefault() // 기본 스크롤 동작 방지
@@ -18,5 +19,11 @@ document.addEventListener('DOMContentLoaded', function () {
         } else {
             header.classList.remove('blur')
         }
+
+        // 스크롤에 맞춰 이미지 회전
+        const maxScrollLeft = horizontalScroll.scrollWidth - windowWidth
+        const scrollPercent = horizontalScroll.scrollLeft / maxScrollLeft
+        const rotateAngle = scrollPercent * 1080 // 회전 각도 계산
+        imgRotate.style.transform = `rotate(${rotateAngle}deg)`
     })
 })
